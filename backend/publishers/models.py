d6ceb22cc9af14e33f28b4c9bb7af09421acad36
6et3ct1ve <6et3ct1ve@gmail.com>
2025-10-07 10:38:23 +0300

    Add publishers models

--- backend/publishers/models.py
+++ backend/publishers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    website = models.URLField()
+    established = models.IntegerField()
+    country = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
