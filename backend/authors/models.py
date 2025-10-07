e69f4e29ef98e4673e65abcbc7dbf5ba5124e97f
6et3ct1ve <6et3ct1ve@gmail.com>
2025-10-07 10:37:45 +0300

    Add authors models

--- backend/authors/models.py
+++ backend/authors/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    biography = models.TextField()
+    birth_year = models.IntegerField()
+    nationality = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
