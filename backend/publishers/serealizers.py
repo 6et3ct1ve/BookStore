7417a7666984bfb5ece6d47d7a138eb2a3c75d24
6et3ct1ve <6et3ct1ve@gmail.com>
2025-10-07 19:13:01 +0300

    Remove wrong named file

--- backend/publishers/serealizers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Publisher
-
-
-class PublisherSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Publisher
-        fields = ["id", "name", "description", "website", "established", "country"]
-        read_only_fields = ["id"]
