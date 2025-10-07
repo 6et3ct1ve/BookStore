70507c765aece61cd52337bf5a65543743c6b7da
6et3ct1ve <6et3ct1ve@gmail.com>
2025-10-07 16:58:33 +0300

    Add books models

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from authors.models import Author
+from publishers.models import Publisher
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT, related_name="books")
+    publisher = models.ForeignKey(
+        Publisher, on_delete=models.PROTECT, related_name="books"
+    )
+    isbn = models.CharField(max_length=13, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    genre = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    pages = models.IntegerField()
+    language = models.CharField(max_length=50)
+    stock_quantity = models.IntegerField(default=0)
+    cover_image = models.URLField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["title"]
