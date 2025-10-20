f6946c123cf5a190050f8b54bd5fba172ea75723
6et3ct1ve <6et3ct1ve@gmail.com>
2025-10-20 12:34:08 +0300

    Add app js and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/js/Layout';
+import BooksPage from './pages/BooksPage';
+import AuthorsPage from './pages/AuthorsPage';
+import PublishersPage from './pages/PublishersPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<BooksPage />} />
+          <Route path="/authors" element={<AuthorsPage />} />
+          <Route path="/publishers" element={<PublishersPage />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
